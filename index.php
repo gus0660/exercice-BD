ca430fe6cb73824e556fcfc6d28d28b4f6c00579
gus0660 <guillaumeroch3@gmail.com>
2024-03-19 10:11:49 +0100

    commentaire

--- index.php
+++ index.php
@@ -85,8 +85,8 @@
                 echo $user['Nom'] . "<br>";
             }
 
-// partie pour le bouton pour vider la base de données, pour suprimer ce bouton,suprimer le code qui suit,
-// 
+// partie pour le bouton pour vider la base de données, pour suprimer ce bouton, suprimer le code qui suit,
+// ne pas oublier de suprimer aussi le script "confirmDelete" dans le head et le "button" html
             // Vérifie si le bouton pour vider la base de données a été cliqué
             if (isset($_POST['clearDB'])) {
                 // Requête SQL pour vider la table
