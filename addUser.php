a4b50bf92784acb3e1129d4126c623a5cf8802f0
gus0660 <guillaumeroch3@gmail.com>
2024-03-19 15:07:34 +0100

    ok pour dernieres modifs

--- addUser.php
+++ addUser.php
@@ -1,2 +1,40 @@
 <?php
+// Vérifie si le formulaire à été soumis
+if (isset($_POST['submit'])) {
 
+    // Stocke la valeur entrée par l'utilisateur dans une variable
+    if (!empty($_POST['nameInput']) && !empty($_POST['emailInput']) && !empty($_POST['passInput'])) {
+        $nameInput = $_POST['nameInput'];
+        $emailInput = $_POST['emailInput'];
+        $passInput = password_hash($_POST['passInput'], PASSWORD_DEFAULT);
+    } else {
+        // echo "<script>alert('Veuillez remplir tous les champs');</script>";
+        header("Location: index.php");
+        exit;
+    }
+    require 'db.php';
+    $qstmt = $bdd->prepare("SELECT COUNT(*) FROM liste_utilisateurs WHERE nom = :nameInput");
+    $qstmt->bindParam(':nameInput', $nameInput);
+    $qstmt->execute();
+    
+    $result = $qstmt->fetch();
+    if ($result['COUNT(*)'] > 0) {
+        echo "<script>alert('Nom déjà utilisé');</script>";
+    } else {
+        // Prépare la requète SQL pour insérer le nom dans la base de donnée
+        $sql = "INSERT INTO liste_utilisateurs (Nom, email, password) VALUES (:nom, :email, :password)";
+        $req = $bdd->prepare($sql);
+
+        // lie le paramètre nom à la valeur entrée par l'utilisateur
+        $req->bindValue(':nom', $nameInput);
+        // lie le paramètre email à la valeur entrée par l'utilisateur
+        $req->bindValue(':email', $emailInput);
+        // lie le paramètre password à la valeur entrée par l'utilisateur
+        $req->bindValue(':password', $passInput);
+
+        // Exécute la requête
+        $req->execute();
+        
+    }
+    header("Location: index.php?success=1");
+}
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require 'db.php';
+if (isset($_GET['success']) && $_GET['success'] == 1) {
+    echo "<script>alert('Inscription réussie');</script>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,7 +21,7 @@ require 'db.php';
 
 <body>
     <div class="container">
-        <form class="m-5" action="#" method="post">
+        <form class="m-5" action="addUser.php" method="post">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="form-group text-center">
@@ -62,43 +65,7 @@ require 'db.php';
 
             <?php
 
-            // Vérifie si le formulaire à été soumis
-            if (isset($_POST['submit'])) {
-
-                // Stocke la valeur entrée par l'utilisateur dans une variable
-                if (!empty($_POST['nameInput']) && !empty($_POST['emailInput']) && !empty($_POST['passInput'])) {
-                    $nameInput = $_POST['nameInput'];
-                    $emailInput = $_POST['emailInput'];
-                    $passInput = password_hash($_POST['passInput'], PASSWORD_DEFAULT);
-                } else {
-                    // echo "<script>alert('Veuillez remplir tous les champs');</script>";
-                    header("Location: " . $_SERVER['PHP_SELF']);
-                    exit;
-                }
-                $qstmt = $bdd->prepare("SELECT COUNT(*) FROM liste_utilisateurs WHERE nom = :nameInput");
-                $qstmt->bindParam(':nameInput', $nameInput);
-                $qstmt->execute();
-                $result = $qstmt->fetch();
-                if ($result['COUNT(*)'] > 0) {
-                    echo "<script>alert('Nom déjà utilisé');</script>";
-                } else {
-                    // Prépare la requète SQL pour insérer le nom dans la base de donnée
-                    $sql = "INSERT INTO liste_utilisateurs (Nom, email, password) VALUES (:nom, :email, :password)";
-                    $req = $bdd->prepare($sql);
-
-                    // lie le paramètre nom à la valeur entrée par l'utilisateur
-                    $req->bindValue(':nom', $nameInput);
-                    // lie le paramètre email à la valeur entrée par l'utilisateur
-                    $req->bindValue(':email', $emailInput);
-                    // lie le paramètre password à la valeur entrée par l'utilisateur
-                    $req->bindValue(':password', $passInput);
-
-                    // Exécute la requête
-                    $req->execute();
-                    echo "Inscription effectuée avec succès : ";
-                }
-                // header("Location: " . $_SERVER['PHP_SELF']);
-            }
+            
 
 
             // partie pour le bouton pour vider la base de données, décommenter les parties adéquates
