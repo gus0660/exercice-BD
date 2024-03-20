49fdaee7eaf2f37410fefb3690779b792c8b971d
gus0660 <guillaumeroch3@gmail.com>
2024-03-20 09:17:01 +0100

    copié/collé code Pierre dans menus.php

---
+++ pages/partial/menu.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Espace Membre</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profil">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login">Connexion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="controllers/logout.php">Déconnexion</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
