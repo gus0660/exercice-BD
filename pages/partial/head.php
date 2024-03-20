5c054fc2592820107eb557cc2747e0955e6831be
gus0660 <guillaumeroch3@gmail.com>
2024-03-20 09:14:59 +0100

    copié/collé code pierre dans head.php

---
+++ pages/partial/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <title><?= $title ?></title> <!-- Affichage du titre de la page -->
+</head>
+<body>
