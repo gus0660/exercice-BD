0572a9ed37287da392478346b42d64512ed9ba37
gus0660 <guillaumeroch3@gmail.com>
2024-03-28 09:30:38 +0100

    déplacement h1 dans admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -5,7 +5,7 @@ logedIn(); // Appel de la fonction de connexion
 $title = 'ADMIN'; // Déclaration du titre de la page
 
 
-  echo "<h1>Bienvenue sur la page ADMINISTRATION</h1>";
+  echo "";
   
 
 
@@ -16,10 +16,10 @@ $user = $_SESSION['profil']; // Récupération des données de l'utilisateur
 include 'partials/head.php'; // Inclusion du fichier d'en-tête
 ?>
 <section class="container text-center">
-  
+  <h1>Bienvenue sur la page ADMINISTRATION</h1>
   <?php include 'partials/menu.php'; ?>
   <div class="border border-3 m-3">
-    <h1 class="d-inline-block border border-3 m-3">Coucou <?= $user['name'] ?></h1>
+    <h1 class="d-inline-block border border-3 m-3">Bonjours <?= $user['name'] ?></h1>
     <pre class="border border-3 m-3">id user : <?= $user['id'] ?></pre>
     <p class="border border-3 m-3"> <?= $user['email'] ?></p>
     <p>Date d'inscription : <?= isset($user['dateInscription']) ? $user['dateInscription'] : 'Non disponible' ?></p>
